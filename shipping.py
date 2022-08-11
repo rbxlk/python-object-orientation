036360da427134b4de43d7c3ee38e8de95037274
R.Luck <rbxlk@protonmail.com>
2022-08-11 14:46:54 +0100

    Add class methods

--- shipping.py
+++ shipping.py
@@ -1,11 +1,21 @@
+import iso6346
+
 class ShippingContainer:
     next_serial = 1337
 
-    @staticmethod
-    def _generate_serial():
-        result = ShippingContainer.next_serial
-        ShippingContainer.next_serial += 1
+    @classmethod
+    def _generate_serial(cls):
+        result = cls.next_serial
+        cls.next_serial += 1
         return result
+    
+    @classmethod
+    def create_empty(cls, owner_code):
+        return cls(owner_code, contents=[])
+
+    @classmethod
+    def create_with_items(cls, owner_code, items):
+        return cls(owner_code, contents=list(items))
 
     def __init__(self, owner_code, contents):
         self.owner_code = owner_code
