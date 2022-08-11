a6b13d9a57988806a53494f245c27b62e2731457
R.Luck <rbxlk@protonmail.com>
2022-08-11 14:53:55 +0100

    Add static method for bic_code generation

--- shipping.py
+++ shipping.py
@@ -9,6 +9,13 @@ class ShippingContainer:
         cls.next_serial += 1
         return result
     
+    @staticmethod
+    def _make_bic_code(owner_code, serial):
+        return iso6346.create(
+            owner_code=owner_code,
+            serial=str(serial).zfill(6)
+        )
+
     @classmethod
     def create_empty(cls, owner_code):
         return cls(owner_code, contents=[])
@@ -20,4 +27,7 @@ class ShippingContainer:
     def __init__(self, owner_code, contents):
         self.owner_code = owner_code
         self.contents = contents
-        self.serial = ShippingContainer._generate_serial()
+        self.bic = ShippingContainer._make_bic_code(
+            owner_code=owner_code,
+            serial=ShippingContainer._generate_serial()
+        )
